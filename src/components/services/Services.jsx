036cb408239e84889925a9dbe70959ca5dee3351
Abhishek-Singh02 <abhishek9502as@gmail.com>
2022-07-14 13:43:13 +0530

    Services Component Done

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,10 +1,115 @@
 import React from 'react'
 import "./Services.css"
+import {IoCheckmarkDoneOutline} from "react-icons/io5"
 
 function Services() {
   return (
-    <section id='Services'>Services</section>
-  )
+    <section id="Services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>Graphics Design</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <IoCheckmarkDoneOutline className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet.</p>
+            </li>
+            <li>
+              <IoCheckmarkDoneOutline className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet.</p>
+            </li>
+            <li>
+              <IoCheckmarkDoneOutline className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet.</p>
+            </li>
+            <li>
+              <IoCheckmarkDoneOutline className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet.</p>
+            </li>
+            <li>
+              <IoCheckmarkDoneOutline className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet.</p>
+            </li>
+            <li>
+              <IoCheckmarkDoneOutline className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet.</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* END OF GRAPHICS DESIGN */}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <IoCheckmarkDoneOutline className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet.</p>
+            </li>
+            <li>
+              <IoCheckmarkDoneOutline className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet.</p>
+            </li>
+            <li>
+              <IoCheckmarkDoneOutline className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet.</p>
+            </li>
+            <li>
+              <IoCheckmarkDoneOutline className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet.</p>
+            </li>
+            <li>
+              <IoCheckmarkDoneOutline className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet.</p>
+            </li>
+            <li>
+              <IoCheckmarkDoneOutline className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet.</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* END OF WEB DEVELOPMENT */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <IoCheckmarkDoneOutline className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet.</p>
+            </li>
+            <li>
+              <IoCheckmarkDoneOutline className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet.</p>
+            </li>
+            <li>
+              <IoCheckmarkDoneOutline className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet.</p>
+            </li>
+            <li>
+              <IoCheckmarkDoneOutline className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet.</p>
+            </li>
+            <li>
+              <IoCheckmarkDoneOutline className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet.</p>
+            </li>
+            <li>
+              <IoCheckmarkDoneOutline className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default Services
